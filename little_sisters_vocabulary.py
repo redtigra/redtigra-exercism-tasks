860302f24de1a3b289d5293beee84a4ce9cd5fcf
Zhenya <redtigrareg@gmail.com>
2022-11-01 20:47:17 +0100

    Initial commit for https://exercism.org/tracks/python/exercises/little-sisters-vocab

---
+++ little_sisters_vocabulary.py
@@ -0,0 +1,25 @@
+"""Functions for creating, transforming, and adding prefixes to strings.
+source: https://exercism.org/tracks/python/exercises/little-sisters-vocab
+"""
+
+
+def add_prefix_un(word):
+    return f"un{word}"
+
+
+def make_word_groups(vocab_words):
+    prefix = vocab_words[0]
+    return prefix + " :: " + " :: ".join([prefix + word for word in vocab_words[1:]])
+
+
+def remove_suffix_ness(word):
+    origin = list(word)[:-4]
+    if origin[-1] == 'i':
+        origin[-1] = 'y'
+    return ''.join(origin)
+
+
+def adjective_to_verb(sentence, index):
+    phrase = sentence.split()
+    word = phrase[index].strip(".")
+    return f"{word}en"

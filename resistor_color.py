4e0777623b8a41ea3088e4bbff0690b89e511195
Zhenya <redtigrareg@gmail.com>
2022-11-28 19:17:33 +0100

    Initial commit for https://exercism.org/tracks/python/exercises/little-sisters-vocab

---
+++ resistor_color.py
@@ -0,0 +1,21 @@
+def color_code(color):
+    return colors().index(color)
+    pass
+
+
+def colors():
+    return [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+    pass
+
+print (color_code("white"))
